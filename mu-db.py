abfe31318513e57949ead68428b3006b88b9502c
aeroheim <benben123213@gmail.com>
2016-02-01 05:50:15 -0600

    basic search functionality

--- mu-db.py
+++ mu-db.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, jsonify, request
-from db import models
+from db import models as db
+from sqlalchemy import or_, distinct
 from api.api import app
-from api.resources.album import getAllAlbums
+from api.resources.album import getAllAlbums, getAlbumData
 
 @app.route('/')
 @app.route('/index')
@@ -12,12 +13,26 @@ def index():
 def search():
     query = request.args.get('query')
     
-    # empty query, return all albums
+    # Empty query, return all albums.
     if not query:
-        result = getAllAlbums()
-        return result;
+        return getAllAlbums()
 
-    return "TESTING"
+    session = db.loadSession()
+    
+    album_ids = session.query(distinct(db.Album.id))\
+                 .select_from(db.Artist)\
+                 .join(db.Album)\
+                 .join(db.AlbumGenres)\
+                 .join(db.Genre)\
+                 .filter(or_(db.Album.album_name.contains(query), db.Artist.artist_name.contains(query), db.Genre.genre_name.contains(query)))\
+                 .all()
+
+    session.close()
+
+    album_ids = map(lambda x: x[0], album_ids)
+
+    # Return all albums whose artist's name, genres, or album name contains the query.
+    return jsonify(albums = [getAlbumData(album_id) for album_id in album_ids])
 
 if __name__ == '__main__':
     app.run(debug=True)
